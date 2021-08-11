4447bbfa81531c230d2b3f259f6ac966169fbc3b
Kazuya-Sakashita <kz0508@gmail.com>
2021-08-11 16:14:13 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+  sessions: 'admins/sessions'
+}
+
+
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
