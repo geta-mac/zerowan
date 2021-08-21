c52d038ee4814573348a54a76336fc3f03934a04
geta-mac <ige1tah6iro1to0@gmail.com>
2021-08-21 11:03:12 +0900

    マイグレーションファイルの順番変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_15_083737) do
+ActiveRecord::Schema.define(version: 2021_08_21_015802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2021_08_15_083737) do
   end
 
   create_table "dm_rooms", force: :cascade do |t|
-    t.bigint "pet_id", null: false
     t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
     t.integer "send_user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
