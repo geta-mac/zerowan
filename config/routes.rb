691a2a92ef820e8cf579dd45a12a7d73d5cafd5b
Kazuya-Sakashita <kz0508@gmail.com>
2021-09-06 06:56:29 +0900

    admins login

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,20 @@
 Rails.application.routes.draw do
+
   devise_for :admins, controllers: {
-  sessions: 'admins/sessions'
+    sessions: 'admins/sessions',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations'  
   }
 
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions',
-    :passwords => 'users/passwords',
-    :confirmations => 'users/confirmations',
-    :unlocks => 'users/unlocks',
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'  
   }
-
-  devise_scope :user do
-    get "signup", :to => "users/registrations#new"
-    get "verify", :to => "users/registrations#verify"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-  end
 
   root 'top#index'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update, :destroy] do
     collection do
