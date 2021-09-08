a3ed892c2d4cfac6a1d7a72f0b1c5f6059c2fb03
saekato <sassasssassssae@gmail.com>
2021-09-08 20:15:50 +0900

    base_controller

--- app/controllers/admins/base_controller.rb
+++ app/controllers/admins/base_controller.rb
@@ -1,2 +1,21 @@
 class Admins::BaseController < ApplicationController
+  before_action :check_admins #全体に適用
+  layout 'admins/layouts/application'
+
+  private
+
+  #ログインしてないとき実行される（sorceryのメソッド）
+  def not_authenticated
+    flash[:warning] = 'ログインしてください'
+    redirect_to admins_sign_up
+  end
+
+  #管理者権限がないユーザーを弾く
+  def check_admins
+    redirect_to root_path, warning: '権限がありません' unless current_user.admins?
+  end
+
+  def show
+  end
+
 end
