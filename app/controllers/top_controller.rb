187d9427e96f83e1eac58e96add4d4bb1864f36a
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-11 20:06:32 +0900

    searchメソッド実行後の@petsを定義※現場検索後は検索前の状態に戻せません

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -11,6 +11,16 @@ class TopController < ApplicationController
 
   def search
     @results = @q.result
+    @pets = Pet.where(category:params[:q][:category])
+      .where(animal_type:params[:q][:animal_type])
+      .where(gender:params[:q][:gender])
+      .where(avairable_area:params[:q][:avairable_area])
+      .where(age:params[:q][:age])
+
+    respond_to do |format|
+      format.html { render :index }
+      format.js
+    end
   end
 
   private
