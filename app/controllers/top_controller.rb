845992fc81cb5c8ebf86e32de24f099d3177dcf9
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-11 19:16:58 +0900

    searchメソッド実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,21 @@
 class TopController < ApplicationController
-    def index
-        @pets = Pet.all
-    end
+  before_action :set_q, only: [:index, :search]
 
-    def show
-        @pet = Pet.find(params[:id])
-    end
+  def index
+    @pets = Pet.all
+  end
 
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def search
+    @results = @q.result
+  end
+
+  private
+
+  def set_q
+    @q = Pet.ransack(params[:q])
+  end
 end
