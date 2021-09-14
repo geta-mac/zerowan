11f3873d81ebe9ca4b45bd656b04526c0c3a29ce
saekato <sassasssassssae@gmail.com>
2021-09-14 19:09:50 +0900

    testcommit

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
-    registrations: 'users/registrations'
+  ギt  registrations: 'users/registrations'
   }
 
   root 'top#index'
