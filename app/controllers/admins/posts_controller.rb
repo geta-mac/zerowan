e78eab23fb36b75adeb9b6d535aa101e124edbf0
Kazuya-Sakashita <kz0508@gmail.com>
2021-09-16 06:15:46 +0900

    admin post modify

--- app/controllers/admins/posts_controller.rb
+++ app/controllers/admins/posts_controller.rb
@@ -1,10 +1,15 @@
 class Admins::PostsController < ApplicationController
   def index
-    @pets = Pet.all
+    @pet_user_postcount = Pet.group(:user_id).count
   end
 
   def show
+    # groupはカテゴリ毎に集計したい時に使用
+    @pet_post_user_ids = Pet.group(:user_id).count.keys
+    @pet_posts = Pet.where(user_id: @pet_post_user_ids)
+
   end
+
 
   def edit
   end
