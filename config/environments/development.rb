ed6c44ef239db1b51c9dd95f453ed2eba2229bf7
geta-mac <ige1tah6iro1to0@gmail.com>
2021-10-24 15:02:57 +0900

    blockedhostエラー解消用

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,8 +69,10 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # Blocked hostエラーの解消用
+  config.hosts << "zerowan.herokuapp.com"
 end
